a53616f8886bb6501ce1854755392c23970a6a88
Martin <martin.teulon@cegetel.net>
2022-05-14 20:01:20 +0200

    Started creation of primary functions

--- env/UART/api/uart.h
+++ env/UART/api/uart.h
@@ -1,6 +1,10 @@
 #ifndef _UART_H
 #define _UART_H
 
-int8_t uart_send_message(char *m, uint32_t i_size);
+int8_t uart_init(uint32_t i_baudrate);
+
+int8_t uart_send_message(char *m, uint16_t i_size);
+
+int8_t uart_read(char *m, uint16_t *o_size);
 
 #endif /* _UART_H */
--- env/UART/src/uart.c
+++ env/UART/src/uart.c
@@ -1,10 +1,17 @@
 /* Global includes */
-#include <stdint.h>
+#include <stdint.h> /* int8_t */
 
 /* Local includes */
 #include "uart.h"
 
-int8_t uart_send_message(char *m, uint32_t i_size)
+int8_t uart_init(uint32_t i_baudrate)
+{
+    int8_t ret = 0;
+
+    return ret;
+}
+
+int8_t uart_send_message(char *m, uint16_t i_size)
 {
     int8_t ret = 0;
 
