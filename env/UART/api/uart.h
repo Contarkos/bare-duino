654403fd51d38e420da8b76517ae1de15bf5e3d3
Martin <martin.teulon@cegetel.net>
2024-05-19 17:36:18 +0200

    Ajout drivers UART

--- env/UART/api/uart.h
+++ env/UART/api/uart.h
@@ -1,10 +1,19 @@
 #ifndef _UART_H
 #define _UART_H
 
-int8_t uart_init(uint32_t i_baudrate);
+typedef struct {
+    volatile uint8_t *ubrrh;
+    volatile uint8_t *ubrrl;
+    volatile uint8_t *ucsra;
+    volatile uint8_t *ucsrb;
+    volatile uint8_t *ucsrc;
+    volatile uint8_t *udr;
+} uart_t_reg;
 
-int8_t uart_send_message(char *m, uint16_t i_size);
+int8_t uart_init(uart_t_reg *i_reg, uint32_t i_baudrate);
 
-int8_t uart_read(char *m, uint16_t *o_size);
+int8_t uart_send_message(uint8_t *m, uint16_t i_size);
+
+int8_t uart_read(uint8_t *m, uint16_t *o_size);
 
 #endif /* _UART_H */
--- env/UART/src/uart.c
+++ env/UART/src/uart.c
@@ -1,6 +1,9 @@
 /* Global includes */
-#include <stdint.h> /* int8_t */
+#include <stdint.h> /* intX_t */
+#include <string.h> /* memset */
 #include <avr/io.h>
+#include <util/delay.h>
+#include <util/atomic.h>
 
 /* Local includes */
 #include "uart.h"
@@ -8,55 +11,172 @@
 /* Defines */
 
 /* We'll just assume the clock is 16MHz (arduino uno mini) */
+#ifndef F_CPU
 #define UART_CLOCK_SPEED   16000000U
+#endif
 
+#define UART_SIZE_BUFFER   256
+
+/* Static definitions */
+static uart_t_reg _uart_reg = { 0 };
+
+static uint8_t _uart_tx_buffer[UART_SIZE_BUFFER];
+static uint16_t _uart_tx_buffer_head;
+static uint16_t _uart_tx_buffer_tail;
+
+static void _uart_write (uint8_t data);
+static uint8_t _uart_receive (void);
+static void _uart_blink(uint16_t ms);
+
+void _uart_tx_irq (void);
 
 static uint32_t _uart_find_baudrate_clock (void)
 {
-   uint32_t clock = UART_CLOCK_SPEED;
-
-   return clock;
+#ifdef F_CPU
+   return F_CPU;
+#else
+   return UART_CLOCK_SPEED;
+#endif
 }
 
-int8_t uart_init(uint32_t i_baudrate)
+int8_t uart_init(uart_t_reg *i_reg, uint32_t i_baudrate)
 {
     int8_t ret = 0;
     uint32_t clock = _uart_find_baudrate_clock();
-    (void) i_baudrate;
+
+    /* Init local variables */
+    _uart_tx_buffer_head = 0;
+    _uart_tx_buffer_tail = 0;
+
+    memset(_uart_tx_buffer, 0, UART_SIZE_BUFFER);
+    memcpy(&_uart_reg, i_reg, sizeof(_uart_reg));
 
     /* Set baudrate based on internal clock */
-    uint16_t brr = ((clock / i_baudrate) >> 4) -1;
+    uint16_t brr = (uint16_t) ((clock / 4 / i_baudrate) - 1) / 2;
+    *_uart_reg.ucsra = (1 << U2X0);
+    //uint16_t baud_setting = (F_CPU / 4 / baud - 1) / 2;
 
-    UBRR0H = (uint8_t) (brr >> 8);
-    UBRR0L = (uint8_t) (brr >> 0);
+    *_uart_reg.ubrrh = (uint8_t) (brr >> 8);
+    *_uart_reg.ubrrl = (uint8_t) (brr >> 0);
 
     /* Enable transmitter and receiver */
-    UCSR0B = (1<<RXEN0) | (1<<TXEN0);
+    *_uart_reg.ucsrb = (1<<RXCIE0) | (1<<RXEN0) | (1<<TXEN0);
     /* Set frame format: 8data, 2stop bit */
-    UCSR0C = (1<<USBS0) | (3<<UCSZ00);
-
-
-    while (!(UCSR0A & (1<<UDRE0)))
-       ;
-    /* Put data into buffer, sends the data */
-    UDR0 = 'a';
+    *_uart_reg.ucsrc = (1<<USBS0) | (3<<UCSZ00);
 
     return ret;
 }
 
-int8_t uart_send_message(char *m, uint16_t i_size)
+int8_t uart_send_message(uint8_t *m, uint16_t i_size)
 {
     int8_t ret = 0;
+    uint16_t ii;
 
     if (0 == m)
         ret = -1;
 
+//    _uart_write('h');
+//    _uart_write('e');
+//    _uart_write('l');
+//    _uart_write('l');
+//    _uart_write('o');
+
     if (0 == ret)
     {
         if (i_size > 0)
-            (void) m;
+        {
+            for (ii = 0; ii < i_size; ii++)
+                _uart_write(m[ii]);
+        }
     }
 
     return ret;
 }
+
+static void _uart_write (uint8_t data)
+{
+    //while ( !(*_uart_reg.ucsra & (1 << UDRE0)) );
+    /* First check if data register is empty and we have nothing 
+       else to write first */
+    if ((_uart_tx_buffer_tail == _uart_tx_buffer_head) && (*_uart_reg.ucsra & (1 << UDRE0)))
+    {
+        ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
+        {
+            *_uart_reg.udr = data;
+            *_uart_reg.ucsra = ((*_uart_reg.ucsra) & ((1 << U2X0) | (1 << MPCM0))) | (1 << TXC0);
+        }
+
+        return;
+    }
+
+    /* Now we can fill the buffer */
+    uint16_t ii = (_uart_tx_buffer_head + 1) % UART_SIZE_BUFFER;
+
+    /* If the buffer is full */
+    while (ii == _uart_tx_buffer_tail)
+    {
+        if (!(SREG & (1 << SREG_I)))
+        {
+            /* Interrupts are disabled, we need to poll */
+            if (*_uart_reg.ucsra & (1 << UDRE0))
+                _uart_tx_irq();
+        }
+        else
+        {
+            /* Nothing to do, interrupts will do the job */
+        }
+    }
+
+    /* Put the data in the buffer */
+    _uart_tx_buffer[_uart_tx_buffer_tail] = data;
+
+    /* Update the head of the buffer atomically to avoid retransmission */
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
+    {
+        _uart_tx_buffer_head = ii;
+        *_uart_reg.ucsrb |= (1 << UDRIE0);
+    }
+}
+
+static uint8_t _uart_receive (void)
+{
+    /* Wait for data to be received */
+    while ( !(*_uart_reg.ucsra & (1 << RXC0)) );
+
+    return *_uart_reg.udr;
+}
+
+static void _uart_blink(uint16_t ms)
+{
+        PORTB |= _BV(PORTB5);
+
+        /*Wait 3000 ms */
+        _delay_ms(ms);
+
+        /*Set to zero the fifth bit of PORTB
+         **Set to LOW the pin 13 */
+        PORTB &= (int8_t) ~_BV(PORTB5);
+
+        /*Wait 3000 ms */
+        _delay_ms(ms);
+}
+
+/* ISR */
+void _uart_tx_irq (void)
+{
+    uint8_t c = _uart_tx_buffer[_uart_tx_buffer_tail];
+    _uart_tx_buffer_tail = (_uart_tx_buffer_tail + 1) % UART_SIZE_BUFFER;
+
+    /* Send the data */
+    *_uart_reg.udr = c;
+    *_uart_reg.ucsra = ((*_uart_reg.ucsra) & ((1 << U2X0) | (1 << MPCM0))) | (1 << TXC0);
+
+    /* disable irq if empty */
+    if (_uart_tx_buffer_head == _uart_tx_buffer_tail)
+    {
+        *_uart_reg.ucsrb &= ~(1 << UDRIE0);
+    }
+}
+
+ISR(USART_UDRE_vect) { _uart_tx_irq(); }
 
